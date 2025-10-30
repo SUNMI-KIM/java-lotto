8739bea2dfa361b5d52f072cb91196a6b40637ac
SUNMI <hariaus@naver.com>
2025-10-30 16:19:39 +0900

    refactor(Validator): 불필요한 protected 생성자 제거

--- src/main/java/lotto/util/validator/Validator.java
+++ src/main/java/lotto/util/validator/Validator.java
@@ -2,10 +2,6 @@ package lotto.util.validator;
 
 public abstract class Validator {
 
-    protected Validator() {
-        // 외부 인스턴스화 방지
-    }
-
     public void validateNotBlank(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException(/* ErrorMessage.EMPTY_INPUT.getMessage() */);
