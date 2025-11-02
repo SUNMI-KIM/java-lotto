7af1e9114e02515c5a13943c719ff36327dd7ba5
SUNMI <hariaus@naver.com>
2025-11-02 23:49:24 +0900

    feat(ErrorMessage): 보너스 번호 중복 예외 메시지 상수 추가

--- src/main/java/lotto/util/exception/ErrorMessage.java
+++ src/main/java/lotto/util/exception/ErrorMessage.java
@@ -9,7 +9,9 @@ public enum ErrorMessage {
 
     INVALID_WINNING_NUMBER_FORMAT("[ERROR] 당첨 번호는 쉼표(,)로 구분된 숫자 형식이어야 합니다."),
     DUPLICATE_WINNING_NUMBER("[ERROR] 당첨 번호는 중복될 수 없습니다."),
-    INVALID_WINNING_NUMBER_SIZE("[ERROR] 당첨 번호는 6개의 숫자로 이루어져야 합니다.");
+    INVALID_WINNING_NUMBER_SIZE("[ERROR] 당첨 번호는 6개의 숫자로 이루어져야 합니다."),
+
+    DUPLICATE_BONUS_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
 
     private final String message;
 
