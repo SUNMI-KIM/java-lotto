f2d80a9e4d83c1d636aab94bb92f1e62002e4984
SUNMI <hariaus@naver.com>
2025-11-02 23:08:26 +0900

    style(InputView): 출력 형식 통일을 위한 공백 라인 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -14,11 +14,13 @@ public class InputView {
     }
 
     public static String inputWinningNumbers() {
+        System.out.println();
         System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
         return Console.readLine();
     }
 
     public static String inputBonusNumber() {
+        System.out.println();
         System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
         return Console.readLine();
     }
